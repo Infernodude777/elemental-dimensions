82c9fd357250d886bfb7994965075e292611cab8
Nikhil Nagaraj <nikhil.p.nagaraj@gmail.com>
2025-11-09 10:49:56 -0800

    Delete 1.21.1 directory

--- 1.21.1/src/main/java/com/nikhil/elementaldimensions/item/undead/BoneShardItem.java
+++
@@ -1,9 +0,0 @@
-package com.nikhil.elementaldimensions.item.undead;
-
-import net.minecraft.item.Item;
-
-public class BoneShardItem extends Item {
-    public BoneShardItem(Settings settings) {
-        super(settings);
-    }
-}
--- 1.21.1/src/main/java/com/nikhil/elementaldimensions/item/undead/EctoplasmItem.java
+++
@@ -1,9 +0,0 @@
-package com.nikhil.elementaldimensions.item.undead;
-
-import net.minecraft.item.Item;
-
-public class EctoplasmItem extends Item {
-    public EctoplasmItem(Settings settings) {
-        super(settings);
-    }
-}
